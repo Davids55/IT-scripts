95081e720260526b8cce7968d9895d7ba5aff618
David Swearingen <83689213+Davids55@users.noreply.github.com>
2026-07-27 15:47:24 -0700

    Delete Event_Log_Parser.ps1

--- Event_Log_Parser.ps1
+++
@@ -1,8 +0,0 @@
-# Script 3 — Event Log Parser
-$since = (Get-Date).AddHours(-24)
-
-Get-WinEvent -FilterHashtable @{
-    LogName = 'System'
-    Level   = 2        # Error
-    StartTime = $since
-} | Select-Object TimeCreated, Id, LevelDisplayName, Message
